26350297474e9e0a79f061b3b59840dad3208c6c
Ditechgit <diogo.jesus.tech@outlook.com>
2024-12-29 00:40:25 -0300

    sem host

--- firebase/public/js/allpages.js
+++
@@ -1,7 +0,0 @@
-/* paginas que nao seja de index deixar a navbar imovel*/
-
-let corpo = document.getElementById('main')
-corpo.style.margin = '200px 0 0 0';
-
-let navbar = document.getElementById('header')
-navbar.style.margin = '0 0 200px 0';
